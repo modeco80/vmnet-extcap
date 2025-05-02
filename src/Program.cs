d7380c30f48108416f971889f0074f11ff1df43a
modeco80 <lily.modeco80@protonmail.ch>
2025-05-01 21:44:19 -0400

    refactor: Just expose seperate VMnet[x] interfaces
    Easier to handle, and removes the kludges I was doing before.

--- src/Program.cs
+++ src/Program.cs
@@ -6,23 +6,13 @@ using ExtcapNet.CaptureInterface;
 using ExtcapNet.PacketPublish;
 using VMNetExtcap;
 using ExtcapNet;
-using System.Globalization;
 
-static uint ParseVMnet(string vmnet) {
-	if (!vmnet.StartsWith("VMnet"))
-		throw new InvalidOperationException("fucking idiot");
-
-	return uint.Parse(vmnet.Substring(5), NumberStyles.Integer);
-}
-
-static void VMWarePacketProducer(Dictionary<ConfigField, string> config, IPacketsPublisher publisher) {
+static void VMWarePacketProducer(uint selectedVMnet, Dictionary<ConfigField, string> config, IPacketsPublisher publisher) {
 	var packetData = new byte[0x640];
 	using var vmNetUserInterface = new VMnetUserInterface();
-	// yeah, I don't understand this library's design choice for this either
-	var selectedVMnet = config.Single(kvp => kvp.Key.DisplayName == "VMnet to capture").Value;
 	var captureStartTime = DateTime.Now;
 
-	vmNetUserInterface.RequestVMnet(ParseVMnet(selectedVMnet));
+	vmNetUserInterface.RequestVMnet(selectedVMnet);
 
 	uint packetLen = 0;
 	while (true) {
@@ -45,28 +35,20 @@ static void VMWarePacketProducer(Dictionary<ConfigField, string> config, IPacket
 	}
 }
 
-// Main
 var extcap = new ExtcapManager();
 
-var bci = new BasicCaptureInterface(displayName: "VMWare VMnet",
-	producer: VMWarePacketProducer,
-	defaultLinkLayer: LinkLayerType.Ethernet);
+// Add VMnet interfaces
+foreach (var id in Enumerable.Range(0, 20)) {
+	var bci = new BasicCaptureInterface(
+		displayName: string.Format("VMware VMnet{0}", id),
+		producer: (config, publisher) => {
+			VMWarePacketProducer((uint)id, config, publisher);
+			return;
+		},
+		defaultLinkLayer: LinkLayerType.Ethernet
+	);
 
-
-bci.AddConfigField(new MultiOptionsField(
-	DisplayName: "VMnet to capture",
-	ConfigField.FieldType.Selector,
-	// I am aware this is batshit insane, but it's less batshit insane
-	// than having to manually do this.
-	Enumerable.Repeat("VMnet", 20)
-		.Zip(Enumerable.Range(0, 20))
-		.Select((a) => {
-			var vmnetName = string.Format("{0}{1}", a.First, a.Second);
-			return new ConfigOption(vmnetName, vmnetName);
-		}).ToList(),
-	Required: true
-));
-
-extcap.RegisterInterface(bci);
+	extcap.RegisterInterface(bci);
+}
 
 extcap.Run(args);
